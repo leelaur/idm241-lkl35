99e7db46e8ce7c6b09ac1d81fd84ac4aeb7a69e3
leelaur <laurenlee552@yahoo.com>
2022-10-08 19:29:08 -0400

    van transition

--- idm241/alpha/build/main.js
+++ idm241/alpha/build/main.js
@@ -4,10 +4,10 @@ const van= document.querySelector ('.van-icon');
 const button=document.querySelector ('button');
 
 button.addEventListener('mouseover', function() {
-    van.style.cssText= 'position:relative;right:50px;transition: 2s ease-in-out;';
+    van.style.cssText= 'transform: translateX(-50px);transition: transform 0.2s;';
   });
 
-  button.addEventListener('mouseout', function() {
-    van.style.cssText= '';
+button.addEventListener('mouseout', function() {
+    van.style.cssText= 'transform: translateX(0);transition: transform 0.2s;';
   });
 
