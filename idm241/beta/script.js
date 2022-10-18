faec1038f3bbfc84c279bfee3dcd28e573f72456
leelaur <laurenlee552@yahoo.com>
2022-10-18 13:45:29 -0400

    take out all input, trying js

--- idm241/beta/script.js
+++ idm241/beta/script.js
@@ -1,9 +1,17 @@
 /* add class and remove */
 const pinObj = document.getElementById('pinBtn');
+const button = document.getElementsByClassName('button')
 
 pinObj.addEventListener ("click", pinFun);
 
+/* TEST
 function pinFun() {
     alert('message');
 }
+*/
 
+/* trying to change color on click */
+function pinFun() {
+    document.button.style.color = "black";
+
+}
