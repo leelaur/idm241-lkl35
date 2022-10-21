5cf0f3a21f4e8453643400d444a2fca123044646
leelaur <laurenlee552@yahoo.com>
2022-10-20 22:06:00 -0400

    beta build update
    using js for click function but wont show add class until after hover. Why?

---
+++ idm241/beta/build/script.js
@@ -0,0 +1,36 @@
+/* add class and remove */
+
+//actual pin image
+const pinObj = document.getElementById('pinBtn');
+//button
+const button = document.getElementById('button');
+
+
+// pinObj.addEventListener ("click", pinFun);
+pinObj.addEventListener('click', () => {
+    pinObj.classList.add('pin-animation');
+
+  })
+
+  button.addEventListener('click', () => {
+    button.classList.add('button-change');
+
+  })
+
+  button.addEventListener.off('click', () => {
+    button.classList.remove('button-change');
+
+  })
+
+// it works but only after hoveroff. WHY??
+
+// function pinFun() {
+//     alert('message');
+// }
+
+/* trying to change color on click */
+// function pinFun() {
+//     button.textContent = 'Animation';
+//     button.classList.add('pin-animation')
+
+// }
