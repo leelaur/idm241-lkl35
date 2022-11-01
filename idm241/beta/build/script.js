7e2e1795bd9d2f13b8bbf6f31daf9c5fadc5c2d3
leelaur <laurenlee552@yahoo.com>
2022-11-01 00:46:07 -0400

    beta build with descriptions

--- idm241/beta/build/script.js
+++ idm241/beta/build/script.js
@@ -7,34 +7,46 @@ const button = document.getElementById('button');
 
 
 // pinObj.addEventListener ("click", pinFun);
-pinObj.addEventListener('click', () => {
-  console.log ('Pin Clicked!');
-    pinObj.classList.add('pin-animation');
+// pinObj.addEventListener('click', () => {
+//   console.log ('Pin Clicked!');
+//     // pinObj.classList.add('pin-animation');
 
-  })
+//   })
+
+
+//HERE
 
   //hover on
   button.addEventListener('mouseenter', () => {
     button.classList.add('button-hover-on');
 
+
   })
   //hover off
   button.addEventListener('mouseleave', () => {
     button.classList.remove('button-hover-on');
+    // button.classList.remove('pinAnim');
+
 
   })
-  //click
-  button.addEventListener('mouseup', () => {
-    button.classList.remove('button-hover-on');
-    button.classList.add('button-click');
 
-  })
-  //click off
-  button.addEventListener('mouseUp', () => {
-    button.classList.toggle('button-hover-off');
-    // button.classList.toggle('button-click');
 
-  })
+
+
+
+
+  // //click
+  // button.addEventListener('mouseup', () => {
+  //   button.classList.remove('button-hover-on');
+  //   button.classList.add('button-click');
+
+  // // })
+  // //click off
+  // button.addEventListener('mouseUp', () => {
+  //   button.classList.toggle('button-hover-off');
+  //   button.classList.toggle('button-click');
+
+  // })
 
   // button.addEventListener.off('click', () => {
   //   button.classList.remove('button-change');
@@ -53,3 +65,34 @@ pinObj.addEventListener('click', () => {
 //     button.classList.add('pin-animation')
 
 // }
+
+
+
+//HERE
+
+// click color
+function clickFunction() {
+  button.classList.toggle ("button-click");
+}
+
+//toggle animation
+button.addEventListener ('click', () => {
+    button.classList.toggle('pinAnim');
+  })
+
+
+
+
+
+
+//ATTEMPT USING BOOLEAN??
+//   const timesClicked = 0;
+//   button.addEventListener ('click', () => {
+//     button.classList.add('pinAnim');
+//     timesClicked++;
+//   })
+
+//   //check if the number is even
+// if(timesClicked % 2 == 0) {
+//   console.log("The number is even.");
+// });
