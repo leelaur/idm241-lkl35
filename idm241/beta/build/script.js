51b077b5e4ae3722f10b6a2c5d9193ea5c141995
leelaur <laurenlee552@yahoo.com>
2022-11-01 22:23:12 -0400

    :white_check_mark: fixed hover off animation
    I did it!!!

--- idm241/beta/build/script.js
+++ idm241/beta/build/script.js
@@ -1,108 +1,63 @@
-/* add class and remove */
-
 //actual pin image
-const pinObj = document.getElementById('pinBtn');
+const pinObj = document.getElementById('pinImg');
 //button
 const buttonObj = document.getElementById('pinButton');
-//  let isPinned = false;
-
-// pinObj.addEventListener ("click", pinFun);
-// pinObj.addEventListener('click', () => {
-//   console.log ('Pin Clicked!');
-//     // pinObj.classList.add('pin-animation');
-
-//   })
 
 
-//HERE
 
   //hover on
   buttonObj.addEventListener('mouseenter', () => {
     buttonObj.classList.add('button-hover-on');
+    buttonObj.classList.remove('button-hover-off');
+    buttonObj.style.cssText= 'display: inline-block; cursor: pointer;';
 
 
   })
-  //hover off
+
+  var gPinnedflag = false;
+
+  // //hover off
   buttonObj.addEventListener('mouseleave', () => {
     buttonObj.classList.remove('button-hover-on');
+    buttonObj.classList.add('button-hover-off');
+    buttonObj.style.removeProperty ('display', 'cursor');
 
-    // if (isPinned == false){
-    //   button.classList.remove('pinAnim');
-    //   isPinned = true;
-    // }
+    if (gPinnedflag == true) {
 
-    // if (isPinned == true) {
-    //   button.classList.add ('pinAnim');
-    //   isPinned = false;
-    // }
-    
-    // button.classList.remove('pinAnim');
-
+      pinObj.classList.remove('playAnim');
+      gPinnedflag = true;
+    }
 
   })
-
-
-
-
-
-
-  // //click
-  // button.addEventListener('mouseup', () => {
-  //   button.classList.remove('button-hover-on');
-  //   button.classList.add('button-click');
-
-  // // })
-  // //click off
-  // button.addEventListener('mouseUp', () => {
-  //   button.classList.toggle('button-hover-off');
-  //   button.classList.toggle('button-click');
-
-  // })
-
-  // button.addEventListener.off('click', () => {
-  //   button.classList.remove('button-change');
-
-  // })
-
-// it works but only after hoveroff. WHY??
-
-// function pinFun() {
-//     alert('message');
-// }
-
-/* trying to change color on click */
-// function pinFun() {
-//     button.textContent = 'Animation';
-//     button.classList.add('pin-animation')
-
-// }
-
-
-
-//HERE
 
 // click color
 
-button.addEventListener ('click', () => {
-    button.classList.toggle ("button-click");
+buttonObj.addEventListener ('click', () => {
+    buttonObj.classList.toggle ('button-click');
+
+      //plays animation only once
+  if (gPinnedflag == false) {
+
+    pinObj.classList.add('playAnim');
+    gPinnedflag = true;
+  }
+  else {
+    gPinnedflag = false;
+  }
   })
 
 
-
-
-var gPinnedflag = false;
-
 //toggle animation
-button.addEventListener ('click', () => {
+// buttonObj.addEventListener ('click', () => {
 
-  //plays animation only once
-  if (gPinnedflag == false) {
+//   //plays animation only once
+//   if (gPinnedflag == false) {
 
-    button.classList.toggle('pinAnim');
-    gPinnedflag = true;
-  }
+//     pinObj.classList.add('pinAnim');
+//     gPinnedflag = true;
+//   }
 
-})
+// })
 
 
 
